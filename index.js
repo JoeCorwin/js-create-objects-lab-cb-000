7d0417f46f7ba04f6890f4a79d8cf6bfcc38436d
Joe Corwin <joe.corwin@gmail.com>
2019-04-16 02:22:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// // Create a constructor function for each of the following:
+// + Dog with `name`, `breed`, and `age` properties
+// + Cat with `name`, `breed`, and `age` properties
+// + Ferret with `name`, `age`, and `foodRequirements` properties
+// + Bird with `name` and `breed` properties
+// + Fish with `name`, `species`, and `waterConditions` properties
+
+function Dog(name, breed, age) {
+  this.dogname = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+
+function Cat(name, breed, age) {
+  this.catname = name;
+  this.breed = breed;
+  this.age = age;
+}
